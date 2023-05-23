ab10abb348f86ebcee810f130f2b6c672065c7a7
Bhuvaneshvar <bhuvaneshvar8@gmail.com>
2023-05-23 10:50:08 +0530

    added custom query

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -5,12 +5,15 @@ import com.example.demo.model.Teacher;
 import com.example.demo.repository.CourseRepository;
 import com.example.demo.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class MainController {
@@ -28,5 +31,10 @@ public class MainController {
     @GetMapping("/courses")
     public List<Course> allCourses() {
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/abc")
+    public ResponseEntity<Teacher> getT(@RequestParam("last") String lastName, @RequestParam("id") Integer id) {
+        return teacherRepository.getTeacher(lastName, id).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- demo/src/main/java/com/example/demo/repository/TeacherRepository.java
+++ demo/src/main/java/com/example/demo/repository/TeacherRepository.java
@@ -2,9 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface TeacherRepository extends JpaRepository<Teacher, Integer
-        > {
+public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+    @Query("from Teacher where lastName = :lastName and id = :id")
+    Optional<Teacher> getTeacher(String lastName, Integer id);
 }
